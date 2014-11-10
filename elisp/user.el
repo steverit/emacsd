87d02433b85762a7fb5837d3fe6001f92cb10dfa
Steve Ritter <steve@seatgeek.com>
2014-11-10 11:32:10 -0500

    initial commit

---
+++ elisp/user.el
@@ -0,0 +1,6 @@
+;; theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'hc-zenburn t)
+
+;; window movements
+(windmove-default-keybindings)
---
+++ init.el
@@ -0,0 +1,19 @@
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+
+;; user settings
+(load "user")
+(load "better-defaults")
+(load "yaml-mode")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("44ae1ffbc978d13cd36dcec321077e888a60bc15990760604324f7fd0e119c50" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
